797ed79195dd1286b26a2516ad15e4182fc33514
Dimusik52 <volodya.pikhulya@mail.ru>
2023-08-25 02:29:45 +0300

    Инициализация репозитория

---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse
+from .models import Advertisement
+from .forms import AdvertisementForm
+
+# Create your views here.
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {"advertisements": advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('index')
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+    context = {'form': form}
+    return render(request, 'advertisement-post.html', context)
